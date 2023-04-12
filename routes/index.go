35f4b37084bc40cd62e516e07173ff662941d165
Eron Alves <eron.a.silva@capgemini.com>
2023-04-12 13:43:56 -0300

    feat: read tasks

--- routes/index.go
+++ routes/index.go
@@ -1,13 +1,16 @@
 package routes
 
 import (
+	"github.com/EronAlves1996/TodoListGo/routes/db"
 	"github.com/gin-gonic/gin"
 )
 
 func Index(c *gin.Context) {
 
+	tasks := db.GetTasks()
+
 	c.HTML(200, "index.tmpl", gin.H{
-		"test": "Test data",
+		"tasks": tasks,
 	})
 
 }
