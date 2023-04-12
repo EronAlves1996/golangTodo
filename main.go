83c3d4defb7d9b5859d72e9fbe24d1dd004e80d4
Eron Alves <eron.a.silva@capgemini.com>
2023-04-12 11:02:51 -0300

    feat: added sqlite db

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
+	"os"
+
 	"github.com/EronAlves1996/TodoListGo/routes"
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	initializeDb()
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 	addRoutes(r)
@@ -15,4 +20,37 @@ func main() {
 func addRoutes(e *gin.Engine) {
 	e.Static("/static", "./static")
 	e.GET("/", routes.Index)
+}
+
+func initializeDb() {
+	file, err := os.Open("./db/tasks.db")
+
+	if err != nil {
+		err := os.Mkdir("db", os.ModePerm)
+
+		if err != nil {
+			panic(err)
+		}
+
+		db, err := sql.Open("sqlite3", "./db/tasks.db")
+
+		if err != nil {
+			panic(err)
+		}
+
+		sqlStmt := `CREATE TABLE tasks (
+			id INTEGER NOT NULL AUTOINCREMENT PRIMARY KEY,
+			description varchar(50) not null,
+			created_at timestamp not null,
+			modified_at timestamp not null,
+			deadline timestamp not null,
+			is_completed boolean not null
+		);`
+
+		if _, err := db.Exec(sqlStmt); err != nil {
+			panic(err)
+		}
+	}
+
+	file.Close()
 }
